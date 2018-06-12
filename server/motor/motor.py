0f68884567cd2748549fef862bc6996cb0315f18
Kye W. Shi <shi.kye@gmail.com>
2018-06-11 17:41:39 -0700

    Start implementing some basic motor controller interface

--- server/motor/motor.py
+++ server/motor/motor.py
@@ -1,15 +1,44 @@
 import thorlabs_apt as apt
 import sys
+import json
 
 
 def send(message):
-    sys.stdout.write('{}\n'.format(message))
+    sys.stdout.write('{}\n'.format(json.dumps(message)))
 
 
-def receive(message):
-    return sys.stdout.readline()
+def receive():
+    return sys.stdin.readline().strip().split(' ')
 
 
-sys.stdout.write('loaded\n')
+# commands
+LIST = 'list'
+CONNECT = 'connect'
+REFRESH = 'refresh'
+MOVE = 'move'
+STATE = 'state'
 
 motors = {sn: model for model, sn in apt.list_available_devices()}
+connected = {}
+
+while True:
+    command, *args = receive()
+
+    if command == LIST:
+        send(list(motors.keys()))
+
+    elif command == CONNECT:
+        sn, = args
+        motor = apt.Motor(sn)
+        connected[sn] = motor
+
+    elif command == REFRESH:
+        apt.APTCleanUp()
+        apt.APTInit()
+
+    elif command == MOVE:
+
+        pass
+
+    elif command == STATE:
+        pass
