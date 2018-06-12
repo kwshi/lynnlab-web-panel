b2c89c95fc03bb8a472deefb2678b512a2a63ce9
Kye W. Shi <shi.kye@gmail.com>
2018-06-11 17:42:10 -0700

    Send groups of messages over websocket rather than single message

--- src/Root/websocket/actions.js
+++ src/Root/websocket/actions.js
@@ -4,12 +4,15 @@ export const RECEIVE_MESSAGE = 'websocket/RECEIVE_MESSAGE';
 export const RECEIVE_LOG = 'websocket/RECEIVE_MESSAGE/RECEIVE_LOG';
 
 
-export const receiveMessage = (json) => {
-    const message = JSON.parse(json.data);
+export const receiveMessage = (json) => (dispatch, store, websocket) => {
+    const messages = JSON.parse(json.data);
+    console.log("message");
 
-    switch (message.type) {
-    case 'log':
-        return receiveLog(message.payload);
+    for (let message of messages) {
+        switch (message.type) {
+        case 'log':
+            dispatch(receiveLog(message.payload));
+        }
     }
 };
 
